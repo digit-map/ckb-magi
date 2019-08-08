578e0a8d63b2ce15e3f3922be1293d316b5620fb
Keith <keithwhisper@gmail.com>
2019-08-08 18:18:47 +0800

    feat: add the type validation on create block dto

--- src/blocks/blocks.controller.ts
+++ src/blocks/blocks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { CreateBlockDto } from './dto/create-block.dto';
 
 @Controller('blocks')
-export class BlocksController {}
+export class BlocksController {
+  @Post()
+  async createBlock(@Body() createBlockDto: CreateBlockDto) {
+    console.log(createBlockDto);
+    return createBlockDto;
+    // todo
+  }
+}
---
+++ src/blocks/dto/create-block.dto.ts
@@ -0,0 +1,47 @@
+import { Type } from 'class-transformer';
+import { IsString, IsNotEmpty, ValidateNested } from 'class-validator';
+
+export class Seal {
+  @IsString()
+  nonce: string;
+  @IsString()
+  proof: string;
+}
+export class Header {
+  @IsString()
+  parentHash: string;
+  @IsString()
+  transactionsRoot: string;
+  @IsString()
+  proposalsHash: string;
+  @IsString()
+  witnessesRoot: string;
+  @IsString()
+  unclesHash: string;
+  @IsString()
+  unclesCount: string;
+  @IsString()
+  dao: string;
+  @IsString()
+  difficulty: string;
+  @IsString()
+  epoch: string;
+  @IsString()
+  hash: string;
+  @IsString()
+  number: string;
+  @IsString()
+  timestamp: string;
+  @IsString()
+  version: string;
+  seal: Seal;
+}
+
+export class CreateBlockDto {
+  @ValidateNested({ each: true })
+  @IsNotEmpty()
+  @Type(() => Header)
+  header: Header;
+  // uncles: []
+  // transactions: []
+}
